74f18dae569d89584bf1299373baaf57d0f4369e
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-01 01:03:03 +0530

    master

--- REST API/routes/subscribers.js
+++ REST API/routes/subscribers.js
@@ -16,8 +16,8 @@ router.get('/', async(req, res) => {
 
 
 // Getting one
-router.get('/:id', (req, res) => {
-    res.send(req.params.id);
+router.get('/:id', getSubscriber, (req, res) => {
+    res.json(res.subscriber);
 });
 
 // Creating one
@@ -35,13 +35,44 @@ router.post('/',async(req, res) => {
 });
 
 // Updating one
-router.patch('/:id', (req, res) => {
-
+router.patch('/:id', getSubscriber, async(req, res) => {
+    if (req.body.name != null){
+        res.subscriber.name = req.body.name;
+    }
+    if (req.body.subscribedToChannel != null){
+        res.subscriber.subscribedToChannel = req.body.subscribedToChannel;
+    }
+    try {
+        const updatedSubscriber = await res.subscriber.save();
+        res.json(updatedSubscriber);
+    } catch (error){
+        res.status(400).json({ message: error.message });
+    }
 });
 
 // Deleting one
-router.delete('/:id', (req, res) => {
-
+router.delete('/:id',getSubscriber, async(req, res) => {
+    try {
+        await res.subscriber.remove();
+        res.json({ message: 'Deleted Subscriber'});
+    } catch (error){
+        res.status(500).json({ message: error.message });
+    }
 });
+
+async function getSubscriber(req, res, next){
+    let subscriber;
+    try {
+        Subscriber = await Subscriber.findById(req.params.id);
+        if (Subscriber == null){
+            return res.status(404).json({ message: 'Cannot find subscriber'});
+        }
+    } catch(err){
+        return res.status(500).json({ message: err.message });
+    }
+
+    res.subscriber = subscriber;
+    next();
+}
 
 module.exports = router;
