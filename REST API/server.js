a46005edfcd96ee2f9e2790bed8756b581579030
arkaslittlemind <arkadiptamojumder14@gmail.com>
2023-09-01 01:09:54 +0530

    master

--- REST API/server.js
+++ REST API/server.js
@@ -1,22 +1,17 @@
-require('dotenv').config();
+require('dotenv').config()
 
-const express = require('express');
-const req = require('express/lib/request');
-const app = express();
-const mongoose = require('mongoose');
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
 
+mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true })
+const db = mongoose.connection
+db.on('error', (error) => console.error(error))
+db.once('open', () => console.log('Connected to Database'))
 
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
-const db = mongoose.connection;
+app.use(express.json())
 
-db.on('error', (error) => console.error(error));
-db.once('open', () => console.log('Connected to Database'));
+const subscribersRouter = require('./routes/subscribers')
+app.use('/subscribers', subscribersRouter)
 
-
-app.use(express.json());
-
-const subscribersRouter = require('./routes/subscribers');
-app.use('/subscribers', subscribersRouter);
-
-
-app.listen(3000, () => console.log('Server running on port 3000')); 
+app.listen(3000, () => console.log('Server Started'))
